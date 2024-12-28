aa057f5668bb1d53a3f776e5fd05b0cfb5d41ef4
callebalik <75130322+callebalik@users.noreply.github.com>
2024-12-28 16:45:16 +0100

    Improved network styling

--- scripts/visNetwork.R
+++ scripts/visNetwork.R
@@ -32,18 +32,19 @@ for (i in seq_len(nrow(co_occurrence_matrix))) {
 }
 
 # Ensure 'group' column exists in node data frame
+
 if (!"group" %in% colnames(nodes)) {
-    nodes$group <- NA # or assign appropriate default values
+  nodes$group <- NA # or assign appropriate default values
 }
 
 # Ensure 'id' column exists in nodes data frame
 if (!"id" %in% colnames(nodes)) {
-    stop("The 'id' column is missing in the nodes data frame.")
+  stop("The 'id' column is missing in the nodes data frame.")
 }
 
 # Ensure 'from' and 'to' columns exist in edges data frame
 if (!all(c("from", "to") %in% colnames(edges))) {
-    stop("The 'from' and 'to' columns are missing in the edges data frame.")
+  stop("The 'from' and 'to' columns are missing in the edges data frame.")
 }
 
 # Ensure 'id' column is unique in nodes data frame
@@ -52,44 +53,45 @@ nodes <- nodes %>% distinct(id, .keep_all = TRUE)
 # Load saved node positions if they exist
 positions_file <- "node_positions.csv"
 if (file.exists(positions_file)) {
-    node_positions <- read.csv(positions_file)
-    if (!"id" %in% colnames(node_positions)) {
-        stop("The 'id' column is missing in the node positions file.")
-    }
-    nodes <- merge(nodes, node_positions, by = "id", all.x = TRUE)
+  node_positions <- read.csv(positions_file)
+  if (!"id" %in% colnames(node_positions)) {
+    stop("The 'id' column is missing in the node positions file.")
+  }
+  nodes <- merge(nodes, node_positions, by = "id", all.x = TRUE)
 } else {
-    node_positions <- data.frame(id = character(), x = numeric(), y = numeric(), stringsAsFactors = FALSE)
+  node_positions <- data.frame(id = character(), x = numeric(), y = numeric(), stringsAsFactors = FALSE)
 }
 
 # Create the network visualization
 network <- visNetwork(nodes, edges, width = "100%") %>%
+  visNodes(
+    shape = "circle",
+    color = list(
+      background = "#67d7b2ad",
+      border = "#0d3d48",
+      highlight = "#ffd760"
+    ),
+    shadow = list(enabled = TRUE, size = 5),
+    font = list(
+      color = "#000000", # Font color
+      size = 11, # Font size
+      face = "calibri" # Font family
+    )
+  ) %>%
+  visEdges(
+    shadow = FALSE,
+    color = list(color = "#36a0c1d8", highlight = "#fe7e7e"),
+    smooth = list(enabled = TRUE, type = "continuous"),
 
-    visNodes(
-        shape = "circle",
-        color = list(
-            background = "#4eecef",
-            border = "#0d3d48",
-            highlight = "#FF8000"
-        ),
-        shadow = list(enabled = TRUE, size = 5),
-        font = list(
-            color = "#000000", # Font color
-            size = 14, # Font size
-            face = "calibri" # Font family
-        )
-    ) %>%
-    visEdges(
-        shadow = FALSE,
-        color = list(color = "#0085AF", highlight = "#e6db0a")
-    ) %>%
-    visOptions(
-        highlightNearest = list(enabled = TRUE, degree = 1, hover = TRUE), # nolint: indentation_linter.
-        selectedBy = "group",
-        nodesIdSelection = TRUE,
-    ) %>%
-    visLayout(randomSeed = 1) %>%
-    visPhysics(stabilization = TRUE) %>%
-    visEvents(stabilized = "function() {
+  ) %>%
+  visOptions(
+    highlightNearest = list(enabled = TRUE, degree = 1, hover = TRUE), # nolint: indentation_linter.
+    selectedBy = "group",
+    nodesIdSelection = TRUE,
+  ) %>%
+  visLayout(randomSeed = 1) %>%
+  visPhysics(stabilization = TRUE) %>%
+  visEvents(stabilized = "function() {
     this.getPositions(function(positions) {
         var positionsData = [];
         for (var id in positions) {
@@ -104,11 +106,11 @@ visSave(network, file = "tests/network.html", selfcontained = FALSE)
 
 # Save node positions after stabilization
 observeEvent(input$node_positions, {
-    new_positions <- do.call(rbind, lapply(input$node_positions, as.data.frame))
-    if (nrow(new_positions) > 0) {
-        write.csv(new_positions, positions_file, row.names = FALSE)
-        node_positions <<- new_positions
-    }
+  new_positions <- do.call(rbind, lapply(input$node_positions, as.data.frame))
+  if (nrow(new_positions) > 0) {
+    write.csv(new_positions, positions_file, row.names = FALSE)
+    node_positions <<- new_positions
+  }
 })
 
 # Assuming node positions are stored in a data frame called 'node_positions'
